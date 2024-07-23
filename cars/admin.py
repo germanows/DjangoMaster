9e9e39cd0658ab1d6c25bf3d5f90e69184ff5694
gege <germano2227@gmail.com>
2024-07-23 09:25:31 -0300

    indo pra main

--- cars/admin.py
+++ cars/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from cars.models import Car
 class carAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
-    search_fields = ('model',)
+    search_fields = ('model','brand')
 
 admin.site.register(Car, carAdmin)
+#diz aqui o que será registrado, e com que parâmetros.
+#os parâmetros são ModelAdmin e o que será registrado, é car.
+
