8e8cb8c63a7b51738c4ba62fce5cdf513dc7ba78
gege <germano2227@gmail.com>
2024-09-23 09:37:10 -0300

    atualizacoes ate a aula 55

--- cars/urls.py
+++ cars/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import cars_view  
 
 urlpatterns = [
-    path('', cars_view, name='cars_view'),  
+    path('', cars_view, name='cars_view'),  # lembrar de definir o nome da URL para o sistema poder chamar a mesma
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from cars.models import Car
+from cars.forms import CarForm
 
 def cars_view(request):
     cars = Car.objects.all()
-    print (cars)
+    search = request.GET.get('search')
+    if search:
+        cars = Car.objects.filter(model__contains=search)
 
-    return render(request, 'cars.html', {'cars': {'model': 'Uno Mile'}})
+    return render(request, 'cars.html', {'cars': cars})
+
+def new_car_view(request):
+    new_car_form = CarForm()
+    return render(request, 'new_car.html', {'new_car_form': new_car_form})
